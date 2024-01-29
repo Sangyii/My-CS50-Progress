7fbebd51a8950f484a2fa48b3bcf93e4b5571d3e
charliepurnomo <charliepurnomo5@gmail.com>
2024-01-29 13:58:54 +0800

    first commit

---
+++ deep.py
@@ -0,0 +1,12 @@
+def main():
+    my_input = input("What is the answer to the great question of life, the universe, and everything? ")
+    thinking(my_input)
+    
+    
+def thinking(inputs):
+        if inputs == "42" or inputs.casefold() == "forty two" or inputs.casefold() == "forty-two":
+            print("Yes")
+        else:
+            print("No")
+
+main()
