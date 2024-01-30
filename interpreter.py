77191f628bca3b98023bc84a9b8cb656737f4d51
charliepurnomo <charliepurnomo5@gmail.com>
2024-01-30 18:21:24 +0800

    first commit

---
+++ interpreter.py
@@ -0,0 +1,26 @@
+def main():
+    main_input = input("Expression: ").strip()
+    interpreter(main_input)
+    
+    
+def interpreter(final):
+    x , y, z = final.split()
+    x = int(x)
+    z = int(z)
+    
+    match y:
+        
+        case "+":
+            print(float(x + z))
+        case "-":
+            print(float(x - z))
+        case "*":
+            print(float(x * z))
+        case "/":
+            print(float(x / z))
+        case _:
+            print("Oops, error")
+                
+            return y
+    
+main()
