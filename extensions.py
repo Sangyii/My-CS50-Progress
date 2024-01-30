12c5a486a1c333e3ae58adf5edf82289be1d679f
charliepurnomo <charliepurnomo5@gmail.com>
2024-01-30 14:49:40 +0800

    first commit

--- extensions.py
+++ extensions.py
@@ -7,9 +7,7 @@ def extension(name = "application/octet-stream"):
     
     if name.casefold().endswith(".gif"):
         print("image", "gif", sep="/")
-    elif name.casefold().endswith(".jpg"):
-        print("image", "jpeg", sep="/")
-    elif name.casefold().endswith(".jpeg"):
+    elif name.casefold().endswith(".jpg", ".jpeg"):
         print("image", "jpeg", sep="/")
     elif name.casefold().endswith(".png"):
         print("image", "png", sep="/")
