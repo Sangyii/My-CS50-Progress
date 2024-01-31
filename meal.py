d360025a8080121ce1d781d0484aac3f844b3211
charliepurnomo <charliepurnomo5@gmail.com>
2024-01-31 16:52:46 +0800

    first commit

--- meal.py
+++ meal.py
@@ -1,23 +1,30 @@
 def main():
     main_input = input("What time is it? ").strip()
     convert(main_input)
+    
+    hours, minutes = convert(main_input).split(".")
+    hours = int(hours)
+    minutes = int(minutes)
+    
+    if 7 <= hours < 8 and 0 <= minutes < 100 or hours == 8 and minutes == 0:
+        print("Breakfast Time")
+    elif 12 <= hours < 13 and 0 <= minutes < 100 or hours == 13 and minutes == 0:
+        print("Lunch Time")
+    elif 18 <= hours < 19 and 0 <= minutes < 100 or hours == 19 and minutes == 0:
+        print("Dinner Time")
+    else:
+        print()       
+    
 
 def convert(time):
     hours, minutes = time.split(":")
     hours = int(hours)
     minutes = int(minutes)
     
-    new_minute = round(float(minutes / 60), 2)
-    formatted_minute = round(new_minute * 60, 1)
+    minutes = round(float(minutes / 60), 2)
+    time_check = float(hours + minutes)
     
-    if 7 <= hours < 8 and 0 <= formatted_minute < 60 or hours == 8 and formatted_minute == 0:
-        print("Breakfast Time")
-    elif 12 <= hours < 13 and 0 <= formatted_minute < 60 or hours == 13 and formatted_minute == 0:
-        print("Lunch Time")
-    elif 18 <= hours < 19 and 0 <= formatted_minute < 60 or hours == 19 and formatted_minute == 0:
-        print("Dinner Time")
-    else:
-        print()        
+    return str(time_check)
     
 
 if __name__ == "__main__":
