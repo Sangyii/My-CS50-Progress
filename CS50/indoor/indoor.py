bd96a23699973832350cf1f11ccf76ad24ac47ff
Sangyii <143314906+Sangyii@users.noreply.github.com>
2024-02-25 17:41:01 +0800

    Delete CS50 directory

--- CS50/indoor/indoor.py
+++
@@ -1,6 +0,0 @@
-def main():
-    my_input = input("").strip().lower()
-    print(my_input)
-
-
-main()
--- CS50/playback/playback.py
+++
@@ -1,7 +0,0 @@
-def main():
-    my_input = input()
-    new_input = my_input.replace(" ", "...")
-    
-    print(f'{new_input}')
-    
-main()
